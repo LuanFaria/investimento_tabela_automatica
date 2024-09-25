f70410fdc701ed9fc75f273c369016fadba38986
LuanFaria <85500922+LuanFaria@users.noreply.github.com>
2024-09-25 11:08:36 -0300

    Update refatora_fiis.py

--- refatora_fiis.py
+++ refatora_fiis.py
@@ -23,16 +23,30 @@ for fiis in fundos:
         By.XPATH, '//*[@id="main-2"]/div[2]/div[1]/div[4]/div/div[1]/strong').text
     v_vp= google.find_element(
         By.XPATH, '/html/body/main/div[2]/div[5]/div/div[2]/div/div[1]/strong').text
+    valor_atual = google.find_element(
+        By.XPATH, '//*[@id="main-2"]/div[2]/div[1]/div[1]/div/div[1]/strong').text
+    varia = google.find_element(
+        By.XPATH, '//*[@id="main-2"]/div[2]/div[1]/div[5]/div/div[1]/strong').text
+    valor_pa = google.find_element(
+        By.XPATH,'//*[@id="main-2"]/div[2]/div[5]/div/div[1]/div/div[1]/strong').text
+    
     dy = dy.replace(',', '.')
     v_vp = v_vp.replace(',', '.')
+    valor_atual = valor_atual.replace(',', '.')
+    varia = varia.replace(',', '.')
+    valor_pa = valor_pa.replace(',', '.')
+
     google.quit
     print('\033[32m', 'Valores de '+urls_fiis[-6:]+'- OK')
 
-    for rows in dados.iter_rows(min_row=2, max_row=15):
+    for rows in dados.iter_rows(min_row=2, max_row=16):
         
         if rows[0].value == urls_fiis[-6:]:  # Nome do Fundo
             rows[1].value = v_vp  # Valor sobre valor patrimonial
             rows[2].value = dy  # Porcentagem do dividendo anual
+            rows[3].value = valor_atual 
+            rows[4].value = varia
+            rows[5].value = valor_pa #Valor patrimonial por cota
         
     nome_do_arquivo.save('Investimento.xlsx')
 print('Finalizado...')
